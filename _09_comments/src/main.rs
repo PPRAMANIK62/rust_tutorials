8d9de359c2be5f779f4829c98d6983710087c7a0
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-05-26 10:28:45 +0530

    DONE: comments

---
+++ _09_comments/src/main.rs
@@ -0,0 +1,11 @@
+// ============================================
+// Comments
+fn main() {
+    // this is for printing Hello, world!
+    println!("Hello, world!");
+
+    // multiline comments
+    /*
+    In rust, the idiomatic comment style starts with a comment with two slashes, and the comment continues until the end of the line. For comments that extend bewyond a single line, you'll need to include...
+    */
+}
